85dbe37c8becb84589eae1dc828c82131fe6868a
胡丁洋10346053 <hu.dingyang@zte.com.cn>
2024-01-11 09:54:15 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ impl LsofData{
     }
     /// to do get socket fd
     fn get_socket_info(&self,path : String){
-        
+
     }
 
 
@@ -213,3 +213,18 @@ impl LsofData{
     
 }
 
+
+#[test]
+fn test_lsall(){
+    let mut d = LsofData::new();
+    let result = d.file_ls().unwrap();
+    println!("{:?}",result);
+}
+
+#[test]
+fn test_target(){
+    let filepath = "/usr/lib64/librt-2.28.so".to_string();
+    let mut d = LsofData::new();
+    let result = d.target_file_ls(filepath).unwrap();
+    println!("{:?}",result);
+}
